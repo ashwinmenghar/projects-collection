b1bb9e086d8a7bf802fd5fa46ea68efda433b57b
Ashwin_Menghar <83464784+ashwinmenghar@users.noreply.github.com>
2023-11-24 22:22:38 +0530

    Update links.js

--- links.js
+++ links.js
@@ -2,5 +2,4 @@ export const links = {
     'Weather App' : 'https://ashwinmenghar.github.io/weather-app/',
     'To Do App' : 'https://ashwinmenghar.github.io/to-do-app/',
     'Kanban Board' : 'https://ashwinmenghar.github.io/kan-ban-board/',
-    'ashwin' : "ashwin.com"
 }
